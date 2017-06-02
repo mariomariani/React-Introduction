c91f6f266491dd7a9338d6f9774f59aebcf35ded
Oscar Testón Rodríguez <oscar.teston@accedo.tv>
2017-06-02 18:22:10 +0200

    add a key in each video list item

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -6,6 +6,7 @@ const VideoList = (props) => {
     const videoItems = props.videos.map((video) => {
         return (
             <VideoListItem
+                key={video.etag} 
                 video={video}/>
         )
     });
